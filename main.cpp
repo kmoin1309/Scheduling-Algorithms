b4b91b3f5173bc8d7c4f9a7cb32b381bce173685
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2024-04-10 01:39:53 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <string>
+#include "parser.h"
+
+using namespace std;
+
+/** Global Constants **/
+
+
+int main(){
+    return 0;
+}
---
+++ parser.h
@@ -0,0 +1,10 @@
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+#endif
