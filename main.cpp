4131f0f5f761ee9103e1f61485819a796ef580ba
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2024-04-12 19:28:55 +0530

    added Sortig , utility functions and constants

--- main.cpp
+++ main.cpp
@@ -5,8 +5,64 @@
 using namespace std;
 
 /** Global Constants **/
+const string TRACE = "trace";
+const string SHOW_STATISTICS = "stats";
+const string ALGORITHMS[9] = {"", "FCFS", "RR-", "SPN", "SRT", "HRRN", "FB-1", "FB-2i", "AGING"};
+
+bool sortByServiceTime(const tuple<string, int, int> &a, const tuple<string, int, int> &b)
+{
+    return (get<2>(a) < get<2>(b));
+}
+bool sortByArrivalTime(const tuple<string, int, int> &a, const tuple<string, int, int> &b)
+{
+    return (get<1>(a) < get<1>(b));
+}
+
+bool descendingly_by_response_ratio(tuple<string, double, int> a, tuple<string, double, int> b)
+{
+    return get<1>(a) > get<1>(b);
+}
+
+bool byPriorityLevel (const tuple<int,int,int>&a,const tuple<int,int,int>&b){
+    if(get<0>(a)==get<0>(b))
+        return get<2>(a)> get<2>(b);
+    return get<0>(a) > get<0>(b);
+}
+
+void clear_timeline()
+{
+    for(int i=0; i<last_instant; i++)
+        for(int j=0; j<process_count; j++)
+            timeline[i][j] = ' ';
+}
+
+string getProcessName(tuple<string, int, int> &a)
+{
+    return get<0>(a);
+}
+
+int getArrivalTime(tuple<string, int, int> &a)
+{
+    return get<1>(a);
+}
+
+int getServiceTime(tuple<string, int, int> &a)
+{
+    return get<2>(a);
+}
+
+int getPriorityLevel(tuple<string, int, int> &a)
+{
+    return get<2>(a);
+}
+
+double calculate_response_ratio(int wait_time, int service_time)
+{
+    return (wait_time + service_time)*1.0 / service_time;
+}
 
 
 int main(){
+    
     return 0;
 }
