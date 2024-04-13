e8a720a2a8f0c0ca3cb467db14434f742aaf53cf
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2024-04-14 01:23:26 +0530

    Added SRT, HighestRatio Algorithm

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <queue>
 #include "parser.h"
 
+#define all(v) v.begin(), v.end()
+
 using namespace std;
 
 /** Global Constants **/
@@ -144,6 +146,115 @@ void roundRobin(int originalQuantum)
     fillInWaitTime();
 }
 
+void shortestProcessNext()
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // pair of service time and index
+    int j = 0;
+    for (int i = 0; i < last_instant; i++)
+    {
+        while(j<process_count && getArrivalTime(processes[j]) <= i){
+            pq.push(make_pair(getServiceTime(processes[j]), j));
+            j++;
+        }
+        if (!pq.empty())
+        {
+            int processIndex = pq.top().second;
+            int arrivalTime = getArrivalTime(processes[processIndex]);
+            int serviceTime = getServiceTime(processes[processIndex]);
+            pq.pop();
+
+            int temp = arrivalTime;
+            for (; temp < i; temp++)
+                timeline[temp][processIndex] = '.';
+
+            temp = i;
+            for (; temp < i + serviceTime; temp++)
+                timeline[temp][processIndex] = '*';
+
+            finishTime[processIndex] = (i + serviceTime);
+            turnAroundTime[processIndex] = (finishTime[processIndex] - arrivalTime);
+            normTurn[processIndex] = (turnAroundTime[processIndex] * 1.0 / serviceTime);
+            i = temp - 1;
+        }
+    }
+}
+
+void shortestRemainingTime()
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    int j = 0;
+    for (int i = 0; i < last_instant; i++)
+    {
+        while(j<process_count &&getArrivalTime(processes[j]) == i){
+            pq.push(make_pair(getServiceTime(processes[j]), j));
+            j++;
+        }
+        if (!pq.empty())
+        {
+            int processIndex = pq.top().second;
+            int remainingTime = pq.top().first;
+            pq.pop();
+            int serviceTime = getServiceTime(processes[processIndex]);
+            int arrivalTime = getArrivalTime(processes[processIndex]);
+            timeline[i][processIndex] = '*';
+
+            if (remainingTime == 1) // process finished
+            {
+                finishTime[processIndex] = i + 1;
+                turnAroundTime[processIndex] = (finishTime[processIndex] - arrivalTime);
+                normTurn[processIndex] = (turnAroundTime[processIndex] * 1.0 / serviceTime);
+            }
+            else
+            {
+                pq.push(make_pair(remainingTime - 1, processIndex));
+            }
+        }
+    }
+    fillInWaitTime();
+}
+
+void highestResponseRatioNext()
+{
+
+    // Vector of tuple <process_name, process_response_ratio, time_in_service> for processes that are in the ready queue
+    vector<tuple<string, double, int>> present_processes;
+    int j=0;
+    for (int current_instant = 0; current_instant < last_instant; current_instant++)
+    {
+        while(j<process_count && getArrivalTime(processes[j])<=current_instant){
+            present_processes.push_back(make_tuple(getProcessName(processes[j]), 1.0, 0));
+            j++;
+        }
+        // Calculate response ratio for every process
+        for (auto &proc : present_processes)
+        {
+            string process_name = get<0>(proc);
+            int process_index = processToIndex[process_name];
+            int wait_time = current_instant - getArrivalTime(processes[process_index]);
+            int service_time = getServiceTime(processes[process_index]);
+            get<1>(proc) = calculate_response_ratio(wait_time, service_time);
+        }
+
+        // Sort present processes by highest to lowest response ratio
+        sort(all(present_processes), descendingly_by_response_ratio);
+
+        if (!present_processes.empty())
+        {
+            int process_index = processToIndex[get<0>(present_processes[0])];
+            while(current_instant<last_instant && get<2>(present_processes[0]) != getServiceTime(processes[process_index])){
+                timeline[current_instant][process_index]='*';
+                current_instant++;
+                get<2>(present_processes[0])++;
+            }
+            current_instant--;
+            present_processes.erase(present_processes.begin());
+            finishTime[process_index] = current_instant + 1;
+            turnAroundTime[process_index] = finishTime[process_index] - getArrivalTime(processes[process_index]);
+            normTurn[process_index] = (turnAroundTime[process_index] * 1.0 / getServiceTime(processes[process_index]));
+        }
+    }
+    fillInWaitTime();
+}
 
 
 int main(){
